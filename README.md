a1dda94d9631b9fd4f3d9074006270de5ceb1796
vaporoxx <jan.zichler@gmx.de>
2025-07-21 17:40:53 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ad-vertisement
+
+The Ad Nauseam blog bot, brought to you by @megatank58 and @vaporoxx
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use serenity::constants;
+
+fn main() {
+	println!("Gateway v{}", constants::GATEWAY_VERSION);
+}
