d08491d9b4cf70698937f22542e1fce71c13e423
megatank58 <tanumegatank@gmail.com>
2025-07-23 21:35:59 +0530

    feat: timeoutme command (#1)

--- src/bin/deploy.rs
+++ src/bin/deploy.rs
@@ -16,11 +16,21 @@ async fn main() -> Result<()> {
 	let create = CreateCommandOption::new(CommandOptionType::SubCommand, "create", "Creates a new blog");
 	let delete = CreateCommandOption::new(CommandOptionType::SubCommand, "delete", "Deletes your blog");
 
+	let timeoutme = CreateCommand::new("timeoutme")
+		.description("Times you out")
+		.set_options(vec![
+			CreateCommandOption::new(CommandOptionType::String, "time", "Duration of time")
+				.add_string_choice("1 hour", "3600")
+				.add_string_choice("2 hours", "7200")
+				.add_string_choice("3 hours", "10800")
+				.add_string_choice("4 hours", "14400"),
+		]);
+
 	let blog = CreateCommand::new("blog")
 		.description("Commands related to blog management")
 		.set_options(vec![create, delete]);
 
-	guild_id.set_commands(&http, vec![blog]).await?;
+	guild_id.set_commands(&http, vec![blog, timeoutme]).await?;
 
 	Ok(())
 }
